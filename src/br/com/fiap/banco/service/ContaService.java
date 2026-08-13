7273bf2b6185f02cdd387ee5c21e773e00bbd7f2
Igor Hayano <167532390+HayanoIche@users.noreply.github.com>
2026-08-13 12:05:31 -0300

    feat: função de saque

--- src/br/com/fiap/banco/service/ContaService.java
+++ src/br/com/fiap/banco/service/ContaService.java
@@ -40,6 +40,27 @@ public class ContaService
         return retorno;
     }
 
-    // Transferencia
+    // Saque
+    public void saque(long numeroConta, double valor) throws Exception
+    {
+        Conta conta = null;
+
+        for (Conta c : contas)
+        {
+            if (c.getNumero().equals(numeroConta))
+            {
+                conta = c;
+                break;
+            }
+        }
+
+        if (conta == null)
+            throw new Exception("Numero da conta " + numeroConta + " não encontrado");
+
+        if (conta.sacar(valor) == false)
+        {
+            throw new Exception("Saldo insuficiente na conta, saldo: " + conta.getSaldo());
+        }
+    }
 
 }
