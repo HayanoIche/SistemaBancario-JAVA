b8cbc433463244b8646f3706971951fcb55500dc
Igor Hayano <167532390+HayanoIche@users.noreply.github.com>
2026-08-13 11:04:26 -0300

    feat: classe da conta pai

---
+++ src/br/com/fiap/banco/model/Conta.java
@@ -0,0 +1,39 @@
+package br.com.fiap.banco.model;
+
+import java.time.LocalDate;
+
+public class Conta
+{
+    // Atributos
+    private long id;
+    private String numero;
+    private double saldo;
+    private LocalDate dataAbertura;
+
+    private Cliente cliente;
+
+    // Construtores
+    // Cheio
+    public Conta(long id, String numero, double saldo, LocalDate dataAbertura, Cliente cliente)
+    {
+        this.id = id;
+        this.numero = numero;
+        this.saldo = saldo;
+        this.dataAbertura = dataAbertura;
+        this.cliente = cliente;
+    }
+
+    // Vazio
+    public Conta() {}
+
+    // Getters & Setters
+    public long getId() { return id; }
+    public String getNumero() { return numero; }
+    public double getSaldo() { return saldo; }
+    public Cliente getCliente() { return cliente; }
+    public LocalDate getDataAbertura() { return dataAbertura; }
+
+    public void setId(long id) { this.id = id; }
+    public void setNumero(String numero) { this.numero = numero; }
+    public void setDataAbertura(LocalDate dataAbertura) { this.dataAbertura = dataAbertura; }
+}
