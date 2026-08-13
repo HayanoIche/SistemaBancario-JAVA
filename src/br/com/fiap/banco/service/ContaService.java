6bd4750a9c2c7fab734cdd6f4b7b291f1d3229dd
Igor Hayano <167532390+HayanoIche@users.noreply.github.com>
2026-08-13 11:56:36 -0300

    feat: função de abertura de conta

--- src/br/com/fiap/banco/service/ContaService.java
+++ src/br/com/fiap/banco/service/ContaService.java
@@ -1,1 +1,45 @@
 package br.com.fiap.banco.service;
+
+import br.com.fiap.banco.model.Cliente;
+import br.com.fiap.banco.model.Conta;
+import br.com.fiap.banco.model.ContaCorrente;
+import br.com.fiap.banco.model.ContaPoupanca;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ContaService
+{
+    // Gambiarrinha
+    private static long serialId = 1;
+    private static long serialNumeroConta = 10000;
+
+    // Banco de dados exemplo
+    private List<Conta> contas = new ArrayList<>();
+
+    // Abertura de conta
+    public Conta aberturaConta(String tipo, Cliente cliente)
+    {
+        Conta retorno = null;
+
+        LocalDate hoje = LocalDate.now();
+        long idConta = serialId++;
+        long numeroConta = serialNumeroConta++;
+
+        if (tipo.equals("Poupanca"))
+        {
+            retorno = new ContaPoupanca(idConta, Long.toString(numeroConta), 0.0, hoje, cliente);
+        }
+        else
+        {
+            retorno = new ContaCorrente(idConta, Long.toString(numeroConta), 0.0, hoje, cliente);
+        }
+
+        contas.add(retorno);
+        return retorno;
+    }
+
+    // Transferencia
+
+}
